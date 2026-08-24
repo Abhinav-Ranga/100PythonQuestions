df952fd7eaeb9dd7a31e7c085531ff660ac828a9
Abhinav Ranga <abhinavranga2006@gmail.com>
2026-08-24 12:54:47 +0530

    Add files via upload

---
+++ Question23.py
@@ -0,0 +1,25 @@
+a = int(input("enter the number: "))
+b = int(input("enter the number: "))
+
+
+temp = None
+
+temp = a
+a = b
+b = temp
+
+print("a after swapping: ", a)
+
+print("b after swapping: ", b)
+
+
+
+# method 2 
+
+# a = int(input("enter the number: "))
+# b = int(input("enter the number: "))
+
+# a,b = b,a
+# print(a)
+# print(b)
+
