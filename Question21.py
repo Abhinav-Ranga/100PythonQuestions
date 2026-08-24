63adeede6967d9959db9c0933c3cb9058471a9ef
Abhinav Ranga <abhinavranga2006@gmail.com>
2026-08-24 12:42:29 +0530

    Add files via upload

---
+++ Question21.py
@@ -0,0 +1,21 @@
+n =  int(input('Enter the number: '))
+
+if n == 1:
+    new = int(input('Enter the number: '))
+    ctf = new / 30.48
+    print('ur value cm to feet : ',ctf )
+elif n==2 :
+    new1 = int(input('Enter the number: '))
+    ktm =  new1 * 0.621371
+    print('ur value in kilometres to miles : ',ktm )
+elif n==3 :
+    new2 = int(input('Enter the number: '))
+    itd =  new2 * 83
+    print('ur value  from inr to dollar : ',itd )
+elif n==4 :
+    print('exit the program: ')
+else:
+    print('u are writing invalid number: get out of here uneducated')
+    
+    
+    
