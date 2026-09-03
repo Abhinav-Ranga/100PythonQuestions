b426ab755bcad1cc149988e993381c6fa743ccda
Abhinav Ranga <abhinavranga2006@gmail.com>
2026-09-03 09:36:37 +0530

    Add files via upload

---
+++ Question27.py
@@ -0,0 +1,13 @@
+def oddchecker(n):
+
+    for i in range(0,n+1):
+        if i%2 == 0:
+            pass
+        else:
+            print(i)
+        
+
+
+n = int(input('enter the number'))     
+
+oddchecker(n)
